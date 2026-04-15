60e4f38b2a1accdc118ca253a2d1f32f0721b484
jannalyntan <s4056775@student.rmit.edu.au>
2026-04-15 11:53:58 +1000

    update

---
+++ V1/script.js
@@ -0,0 +1,32 @@
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  document.getElementById("name").style.transform = `translateY(${
+    scrollY * 0.05
+  }px)`;
+});
+
+document.addEventListener("scroll", function () {
+  const reveals = document.querySelectorAll(".scroll-reveal");
+
+  reveals.forEach((el, index) => {
+    const elementTop = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight - 100) {
+      // add a slight delay for each item
+      el.style.transitionDelay = `${index * 0.15}s`;
+      el.classList.add("visible");
+    }
+  });
+});
+
+window.addEventListener("load", () => {
+  const images = document.querySelectorAll(".fade-in-on-load");
+
+  images.forEach((img, index) => {
+    // Stagger the reveal slightly
+    img.style.transitionDelay = `${index * 0.2}s`;
+    img.classList.add("visible");
+  });
+});
